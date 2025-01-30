eb270f32d4c89f8458cf0bc3bf9eabc2b3430e64
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-01-30 19:42:02 +0000

    Add htmx counter demo

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,56 @@
 use anyhow::Result;
 use askama_axum::Template;
 use axum::{
+    extract::State,
     routing::{get, post},
     Router,
 };
-use std::net::SocketAddr;
+use std::sync::Arc;
+use std::{
+    net::SocketAddr,
+    sync::atomic::{AtomicU64, Ordering},
+};
 use tokio::net::TcpListener;
 use tower_http::services::ServeDir;
 
+#[derive(Clone)]
+struct AppState {
+    counter: Arc<AtomicU64>,
+}
+
 #[derive(Template)]
 #[template(path = "index.html")]
-struct IndexTemplate {}
+struct IndexTemplate {
+    count: u64,
+}
 
 #[derive(Template)]
 #[template(path = "click_response.html")]
-struct ClickResponseTemplate {}
-
-async fn index_handler() -> IndexTemplate {
-    IndexTemplate {}
+struct ClickResponseTemplate {
+    count: u64,
 }
 
-async fn click_handler() -> ClickResponseTemplate {
-    ClickResponseTemplate {}
+async fn index_handler(State(state): State<AppState>) -> IndexTemplate {
+    let count = state.counter.load(Ordering::Relaxed);
+    IndexTemplate { count }
+}
+
+async fn click_handler(State(state): State<AppState>) -> ClickResponseTemplate {
+    let count = state.counter.fetch_add(1, Ordering::Relaxed) + 1;
+    ClickResponseTemplate { count }
 }
 
 fn create_router() -> Router {
+    // Create shared state
+    let state = AppState {
+        counter: Arc::new(AtomicU64::new(0)),
+    };
+
     Router::new()
         .route("/", get(index_handler))
         .route("/clicked", post(click_handler))
         .nest_service("/static", ServeDir::new("static"))
+        .with_state(state)
 }
 
 #[tokio::main]

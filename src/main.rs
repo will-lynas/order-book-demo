fbcc7c3623b89c70fac44674fa250bb74c9e770d
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-01-30 17:20:24 +0000

    Add axum hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:5900").await?;
+    println!("Listening on http://127.0.0.1:5900");
+    
+    axum::serve(listener, app).await?;
+    Ok(())
 }

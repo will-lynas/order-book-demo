a2cb2653545a16f3d00e74a052d593144d8d25f6
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-01-30 17:23:25 +0000

    Import instead of qualify

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use axum::{routing::get, Router};
+use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }));
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:5900").await?;
+    let listener = TcpListener::bind("127.0.0.1:5900").await?;
     println!("Listening on http://127.0.0.1:5900");
     
     axum::serve(listener, app).await?;

cec0858eddfbcb6e6256eb45bc6ad79abaebb124
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-02-01 12:07:25 +0000

    Store buys in a max heap

--- src/order_book.rs
+++ src/order_book.rs
@@ -1,12 +1,35 @@
+use std::cmp::Ordering;
+use std::collections::BinaryHeap;
+
 #[derive(Clone, Debug)]
 pub struct Entry {
     pub price: f64,
     pub quantity: f64,
 }
 
+impl PartialEq for Entry {
+    fn eq(&self, other: &Self) -> bool {
+        self.price == other.price
+    }
+}
+
+impl Eq for Entry {}
+
+impl Ord for Entry {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.price.total_cmp(&other.price)
+    }
+}
+
+impl PartialOrd for Entry {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
 #[derive(Default)]
 pub struct OrderBook {
-    buy_entries: Vec<Entry>,
+    buy_entries: BinaryHeap<Entry>,
     sell_entries: Vec<Entry>,
 }
 
